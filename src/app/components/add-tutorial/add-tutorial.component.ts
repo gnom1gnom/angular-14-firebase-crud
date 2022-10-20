5e61702e7ca28b030f800d70a21b7d04fb1f488d
Krzysztof Jurkowski <krzysztof.jurkowski@roche.com>
2022-10-20 11:27:46 +0000

    working upload

--- src/app/components/add-tutorial/add-tutorial.component.ts
+++ src/app/components/add-tutorial/add-tutorial.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FileUpload } from 'src/app/models/fileupload.model';
 import { Tutorial } from 'src/app/models/tutorial.model';
 import { TutorialService } from 'src/app/services/tutorial.service';
+
+import { finalize } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-add-tutorial',
@@ -11,6 +15,9 @@ export class AddTutorialComponent implements OnInit {
 
   tutorial: Tutorial = new Tutorial();
   submitted = false;
+  selectedFiles?: FileList;
+  currentFileUpload?: FileUpload;
+  percentage = 0;
 
   constructor(private tutorialService: TutorialService) { }
 
@@ -18,14 +25,56 @@ export class AddTutorialComponent implements OnInit {
   }
 
   saveTutorial(): void {
-    this.tutorialService.create(this.tutorial).then(() => {
-      console.log('Created new item successfully!');
-      this.submitted = true;
+    if (this.selectedFiles) {
+      const file: File | null = this.selectedFiles.item(0);
+      this.selectedFiles = undefined;
+
+      if (file) {
+        this.upload(file);
+      }
+    }
+    else {
+      this.tutorialService.create(this.tutorial).then(() => {
+        console.log('Created new item successfully!');
+        this.submitted = true;
+      });
+    }
+  }
+
+  selectFile(event: any): void {
+    this.selectedFiles = event.target.files;
+  }
+
+  upload(file: File): void {
+
+    this.currentFileUpload = new FileUpload(file);
+    const uploadTracker = this.tutorialService.pushFileToStorage(this.currentFileUpload);
+    uploadTracker.uploadTask.percentageChanges().subscribe(percentage => {
+      this.percentage = Math.round(percentage ? percentage : 0);
     });
+
+
+    uploadTracker.uploadTask.snapshotChanges().pipe(
+      finalize(() => {
+        uploadTracker.storageRef.getDownloadURL().subscribe(downloadURL => {
+          this.tutorial.fileUrl = downloadURL;
+          this.tutorialService.create(this.tutorial).then(() => {
+            console.log('Created new item successfully!');
+            this.submitted = true;
+          });
+        });
+      })
+    ).subscribe();
   }
 
   newTutorial(): void {
     this.submitted = false;
     this.tutorial = new Tutorial();
+    if (this.selectedFiles) delete this.selectedFiles;
+    this.percentage = 0;
   }
+
 }
+
+
+
--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
+import { AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask } from '@angular/fire/compat/storage';
+import { FileUpload } from '../models/fileupload.model';
 import { Tutorial } from '../models/tutorial.model';
 
+import { nanoid } from 'nanoid'
+
+export interface uploadTracker { uploadTask: AngularFireUploadTask, storageRef: AngularFireStorageReference }
 @Injectable({
   providedIn: 'root'
 })
 export class TutorialService {
   private dbPath = '/tutorials';
+  private basePath = '/uploads';
 
   tutorialsRef: AngularFireList<Tutorial>;
 
-  constructor(private db: AngularFireDatabase) {
+  constructor(private db: AngularFireDatabase, private storage: AngularFireStorage) {
     this.tutorialsRef = db.list(this.dbPath);
   }
 
@@ -33,4 +39,33 @@ export class TutorialService {
   deleteAll(): Promise<void> {
     return this.tutorialsRef.remove();
   }
+
+  pushFileToStorage(fileUpload: FileUpload): uploadTracker {
+    const filePath = `${this.basePath}/${nanoid()}`;
+    const storageRef = this.storage.ref(filePath);
+    return {
+      uploadTask: this.storage.upload(filePath, fileUpload.file),
+      storageRef: storageRef
+    }
+  }
+
+  deleteFile(fileUpload: FileUpload): void {
+    if (fileUpload.key)
+      this.deleteFileDatabase(fileUpload.key)
+        .then(() => {
+          this.deleteFileStorage(fileUpload.name);
+        })
+        .catch(error => console.log(error));
+  }
+
+  private deleteFileDatabase(key: string): Promise<void> {
+    return this.tutorialsRef.remove(key);
+  }
+
+  private deleteFileStorage(name?: string): void {
+    const storageRef = this.storage.ref(this.basePath);
+    if (name)
+      storageRef.child(name).delete();
+  }
+
 }
